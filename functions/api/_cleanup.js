13814b9e3094767526d030b0eb81aaac5149ac0b
Peter Sladkarov <peter.sladkarov@users.noreply.github.com>
2026-08-14 01:49:31 +0300

    Remove temp cleanup endpoint

--- functions/api/_cleanup.js
+++
@@ -1,6 +0,0 @@
-export async function onRequest(context) {
-    const { env } = context;
-    if (!env.TEXIM_CALENDAR) return new Response('no kv', { status: 500 });
-    await env.TEXIM_CALENDAR.delete('convoy-88888');
-    return new Response('deleted test convoy', { status: 200 });
-}
